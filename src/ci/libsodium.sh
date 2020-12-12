cad984c6aa9d6efc411010761c37c275e8d9eaa6
Brian Demers <bdemers@apache.org>
2020-12-11 19:03:06 -0500

    Cleaning up CI scripts
    - use `sudo` to install libsodium
    - setting execute bit on libsodium.sh
    - Remove travis ci config

--- src/ci/libsodium.sh
+++ src/ci/libsodium.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
+#
+# Copyright 2020-Present paseto.dev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 echo "installing libsodium"
 curl https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz | tar xvz -C ../
 pushd ../libsodium-1.0.18
 ./configure
 make
-make install
+sudo make install
 popd
