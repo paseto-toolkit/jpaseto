1b04cbfc1cae6c4bc621ff750c6dc205c0591e90
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-20 06:32:52 +0200

    use hamcrest matcher

--- api/src/test/groovy/dev/paseto/jpaseto/paserk/TypeTest.groovy
+++ api/src/test/groovy/dev/paseto/jpaseto/paserk/TypeTest.groovy
@@ -18,110 +18,112 @@ package dev.paseto.jpaseto.paserk
 import dev.paseto.jpaseto.Purpose
 import dev.paseto.jpaseto.UnsupportedPasetoException
 import org.testng.annotations.Test
-import static org.testng.AssertJUnit.*
+import static dev.paseto.jpaseto.Util.expect
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.is
 
 class TypeTest {
 
     @Test
     void testSafeInFooter() {
-        assertEquals(true, Type.LID.isSafeInFooter())
-        assertEquals(false, Type.LOCAL.isSafeInFooter())
-        assertEquals(true, Type.SEAL.isSafeInFooter())
-        assertEquals(true, Type.LOCAL_WRAP.isSafeInFooter())
-        assertEquals(true, Type.LOCAL_PW.isSafeInFooter())
-        assertEquals(true, Type.SID.isSafeInFooter())
-        assertEquals(false, Type.PUBLIC.isSafeInFooter())
-        assertEquals(true, Type.PID.isSafeInFooter())
-        assertEquals(false, Type.SECRET.isSafeInFooter())
-        assertEquals(true, Type.SECRET_WRAP.isSafeInFooter())
-        assertEquals(true, Type.SECRET_PW.isSafeInFooter())
+        assertThat(true, is(Type.LID.isSafeInFooter()))
+        assertThat(false, is(Type.LOCAL.isSafeInFooter()))
+        assertThat(true, is(Type.SEAL.isSafeInFooter()))
+        assertThat(true, is(Type.LOCAL_WRAP.isSafeInFooter()))
+        assertThat(true, is(Type.LOCAL_PW.isSafeInFooter()))
+        assertThat(true, is(Type.SID.isSafeInFooter()))
+        assertThat(false, is(Type.PUBLIC.isSafeInFooter()))
+        assertThat(true, is(Type.PID.isSafeInFooter()))
+        assertThat(false, is(Type.SECRET.isSafeInFooter()))
+        assertThat(true, is(Type.SECRET_WRAP.isSafeInFooter()))
+        assertThat(true, is(Type.SECRET_PW.isSafeInFooter()))
     }
 
     @Test
     void testDataEncoded() {
-        assertEquals(true, Type.LID.isDataEncoded())
-        assertEquals(true, Type.LOCAL.isDataEncoded())
-        assertEquals(true, Type.SEAL.isDataEncoded())
-        assertEquals(false, Type.LOCAL_WRAP.isDataEncoded())
-        assertEquals(true, Type.LOCAL_PW.isDataEncoded())
-        assertEquals(true, Type.SID.isDataEncoded())
-        assertEquals(true, Type.PUBLIC.isDataEncoded())
-        assertEquals(true, Type.PID.isDataEncoded())
-        assertEquals(true, Type.SECRET.isDataEncoded())
-        assertEquals(false, Type.SECRET_WRAP.isDataEncoded())
-        assertEquals(true, Type.SECRET_PW.isDataEncoded())
+        assertThat(true, is(Type.LID.isDataEncoded()))
+        assertThat(true, is(Type.LOCAL.isDataEncoded()))
+        assertThat(true, is(Type.SEAL.isDataEncoded()))
+        assertThat(false, is(Type.LOCAL_WRAP.isDataEncoded()))
+        assertThat(true, is(Type.LOCAL_PW.isDataEncoded()))
+        assertThat(true, is(Type.SID.isDataEncoded()))
+        assertThat(true, is(Type.PUBLIC.isDataEncoded()))
+        assertThat(true, is(Type.PID.isDataEncoded()))
+        assertThat(true, is(Type.SECRET.isDataEncoded()))
+        assertThat(false, is(Type.SECRET_WRAP.isDataEncoded()))
+        assertThat(true, is(Type.SECRET_PW.isDataEncoded()))
     }
 
     @Test
     void testCompatibility() {
-        assertEquals(Purpose.LOCAL, Type.LID.getCompatibility())
-        assertEquals(Purpose.LOCAL, Type.LOCAL.getCompatibility())
-        assertEquals(Purpose.LOCAL, Type.SEAL.getCompatibility())
-        assertEquals(Purpose.LOCAL, Type.LOCAL_WRAP.getCompatibility())
-        assertEquals(Purpose.LOCAL, Type.LOCAL_PW.getCompatibility())
-        assertEquals(Purpose.PUBLIC, Type.SID.getCompatibility())
-        assertEquals(Purpose.PUBLIC, Type.PUBLIC.getCompatibility())
-        assertEquals(Purpose.PUBLIC, Type.PID.getCompatibility())
-        assertEquals(Purpose.PUBLIC, Type.SECRET.getCompatibility())
-        assertEquals(Purpose.PUBLIC, Type.SECRET_WRAP.getCompatibility())
-        assertEquals(Purpose.PUBLIC, Type.SECRET_PW.getCompatibility())
+        assertThat(Purpose.LOCAL, is(Type.LID.getCompatibility()))
+        assertThat(Purpose.LOCAL, is(Type.LOCAL.getCompatibility()))
+        assertThat(Purpose.LOCAL, is(Type.SEAL.getCompatibility()))
+        assertThat(Purpose.LOCAL, is(Type.LOCAL_WRAP.getCompatibility()))
+        assertThat(Purpose.LOCAL, is(Type.LOCAL_PW.getCompatibility()))
+        assertThat(Purpose.PUBLIC, is(Type.SID.getCompatibility()))
+        assertThat(Purpose.PUBLIC, is(Type.PUBLIC.getCompatibility()))
+        assertThat(Purpose.PUBLIC, is(Type.PID.getCompatibility()))
+        assertThat(Purpose.PUBLIC, is(Type.SECRET.getCompatibility()))
+        assertThat(Purpose.PUBLIC, is(Type.SECRET_WRAP.getCompatibility()))
+        assertThat(Purpose.PUBLIC, is(Type.SECRET_PW.getCompatibility()))
     }
 
     @Test
     void testToStringReturnsType() {
-        assertEquals("lid", Type.LID.toString())
-        assertEquals("local", Type.LOCAL.toString())
-        assertEquals("seal", Type.SEAL.toString())
-        assertEquals("local-wrap", Type.LOCAL_WRAP.toString())
-        assertEquals("local-pw", Type.LOCAL_PW.toString())
-        assertEquals("sid", Type.SID.toString())
-        assertEquals("public", Type.PUBLIC.toString())
-        assertEquals("pid", Type.PID.toString())
-        assertEquals("secret", Type.SECRET.toString())
-        assertEquals("secret-wrap", Type.SECRET_WRAP.toString())
-        assertEquals("secret-pw", Type.SECRET_PW.toString())
+        assertThat("lid", is(Type.LID.toString()))
+        assertThat("local", is(Type.LOCAL.toString()))
+        assertThat("seal", is(Type.SEAL.toString()))
+        assertThat("local-wrap", is(Type.LOCAL_WRAP.toString()))
+        assertThat("local-pw", is(Type.LOCAL_PW.toString()))
+        assertThat("sid", is(Type.SID.toString()))
+        assertThat("public", is(Type.PUBLIC.toString()))
+        assertThat("pid", is(Type.PID.toString()))
+        assertThat("secret", is(Type.SECRET.toString()))
+        assertThat("secret-wrap", is(Type.SECRET_WRAP.toString()))
+        assertThat("secret-pw", is(Type.SECRET_PW.toString()))
     }
 
     @Test
     void testOfMethod() {
-        assertFalse Type.of("foo").isPresent()
-        assertTrue Type.of("lid").isPresent()
-        assertTrue Type.of("lid").get() == Type.LID
-        assertTrue Type.of("local").isPresent()
-        assertTrue Type.of("local").get() == Type.LOCAL
-        assertTrue Type.of("seal").isPresent()
-        assertTrue Type.of("seal").get() == Type.SEAL
-        assertTrue Type.of("local-wrap").isPresent()
-        assertTrue Type.of("local-wrap").get() == Type.LOCAL_WRAP
-        assertTrue Type.of("local-pw").isPresent()
-        assertTrue Type.of("local-pw").get() == Type.LOCAL_PW
-        assertTrue Type.of("sid").isPresent()
-        assertTrue Type.of("sid").get() == Type.SID
-        assertTrue Type.of("public").isPresent()
-        assertTrue Type.of("public").get() == Type.PUBLIC
-        assertTrue Type.of("pid").isPresent()
-        assertTrue Type.of("pid").get() == Type.PID
-        assertTrue Type.of("secret").isPresent()
-        assertTrue Type.of("secret").get() == Type.SECRET
-        assertTrue Type.of("secret-wrap").isPresent()
-        assertTrue Type.of("secret-wrap").get() == Type.SECRET_WRAP
-        assertTrue Type.of("secret-pw").isPresent()
-        assertTrue Type.of("secret-pw").get() == Type.SECRET_PW
+        assertThat(false, is(Type.of("foo").isPresent()))
+        assertThat(true, is(Type.of("lid").isPresent()))
+        assertThat(true, is(Type.of("lid").get() == Type.LID))
+        assertThat(true, is(Type.of("local").isPresent()))
+        assertThat(true, is(Type.of("local").get() == Type.LOCAL))
+        assertThat(true, is(Type.of("seal").isPresent()))
+        assertThat(true, is(Type.of("seal").get() == Type.SEAL))
+        assertThat(true, is(Type.of("local-wrap").isPresent()))
+        assertThat(true, is(Type.of("local-wrap").get() == Type.LOCAL_WRAP))
+        assertThat(true, is(Type.of("local-pw").isPresent()))
+        assertThat(true, is(Type.of("local-pw").get() == Type.LOCAL_PW))
+        assertThat(true, is(Type.of("sid").isPresent()))
+        assertThat(true, is(Type.of("sid").get() == Type.SID))
+        assertThat(true, is(Type.of("public").isPresent()))
+        assertThat(true, is(Type.of("public").get() == Type.PUBLIC))
+        assertThat(true, is(Type.of("pid").isPresent()))
+        assertThat(true, is(Type.of("pid").get() == Type.PID))
+        assertThat(true, is(Type.of("secret").isPresent()))
+        assertThat(true, is(Type.of("secret").get() == Type.SECRET))
+        assertThat(true, is(Type.of("secret-wrap").isPresent()))
+        assertThat(true, is(Type.of("secret-wrap").get() == Type.SECRET_WRAP))
+        assertThat(true, is(Type.of("secret-pw").isPresent()))
+        assertThat(true, is(Type.of("secret-pw").get() == Type.SECRET_PW))
     }
 
     @Test
     void testFromMethod() {
         expect UnsupportedPasetoException, { Type.from("foo") }
-        assertTrue Type.from("lid") == Type.LID
-        assertTrue Type.from("local") == Type.LOCAL
-        assertTrue Type.from("seal") == Type.SEAL
-        assertTrue Type.from("local-wrap") == Type.LOCAL_WRAP
-        assertTrue Type.from("local-pw") == Type.LOCAL_PW
-        assertTrue Type.from("sid") == Type.SID
-        assertTrue Type.from("public") == Type.PUBLIC
-        assertTrue Type.from("pid") == Type.PID
-        assertTrue Type.from("secret") == Type.SECRET
-        assertTrue Type.from("secret-wrap") == Type.SECRET_WRAP
-        assertTrue Type.from("secret-pw") == Type.SECRET_PW
+        assertThat(true, is(Type.from("lid") == Type.LID))
+        assertThat(true, is(Type.from("local") == Type.LOCAL))
+        assertThat(true, is(Type.from("seal") == Type.SEAL))
+        assertThat(true, is(Type.from("local-wrap") == Type.LOCAL_WRAP))
+        assertThat(true, is(Type.from("local-pw") == Type.LOCAL_PW))
+        assertThat(true, is(Type.from("sid") == Type.SID))
+        assertThat(true, is(Type.from("public") == Type.PUBLIC))
+        assertThat(true, is(Type.from("pid") == Type.PID))
+        assertThat(true, is(Type.from("secret") == Type.SECRET))
+        assertThat(true, is(Type.from("secret-wrap") == Type.SECRET_WRAP))
+        assertThat(true, is(Type.from("secret-pw") == Type.SECRET_PW))
     }
 }
